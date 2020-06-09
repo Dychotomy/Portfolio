c432edf694c65cbdfc9cf66c0cfc2134110ead4f
dychotomy <dychotomy2000@yahoo.com>
2020-06-09 10:38:24 -0400

    basic connections/styles

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log(`I am connected!`)
+const sheetUrl = 'https://docs.google.com/spreadsheets/d/12ayaNOj50jTxSo0pVE2Rl60CJYdHl0UspqmoAOqjSmY/edit?usp=sharing'
+const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12ayaNOj50jTxSo0pVE2Rl60CJYdHl0UspqmoAOqjSmY/od6/public/values?alt=json'
+$.ajax({
+    url: sheetAsJSON
+}).then( data => {
+    console.log('data', data)
+    const projects = data.feed.entry.map( project => {
+        return {
+            title: project.gsx$title.$t,
+            image: project.gsx$image.$t,
+            description: project.gsx$description.$t,
+            url: project.gsx$url.$t
+        }
+    }) // end of the map
+    app(projects)
+})
+// .catch is for  handling errors
+.catch( (err => console.log('err', err)))
+
+
+console.log(`running after ajax`)
+
+function app(projectsArr) {
+    console.log('app - projectsArr', projectsArr)
+        projectsArr.forEach( project => {
+        let title = $('<h3>')
+        title.text(project.title)
+        $('body').append(title)
+})
+}
